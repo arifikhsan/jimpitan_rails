ef0ad8d823481839ed60f73f97c99674411a831a
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-09-05 09:52:04 +0700

    home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  root 'home#index'
+  get 'home/index'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
